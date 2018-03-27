b4e9330380a1442c0831a19c9198ae420c743f19
Carl Ambroselli <git@carl-ambroselli.de>
2018-03-27 22:19:40 +0300

    add bieber.sh

---
+++ bieber.sh
@@ -0,0 +1,47 @@
+cat <<EOF > ~/.bieber
+while true; do sleep 20; osascript -e "set Volume 10"; say "Trying to hack the"; cat /usr/share/dict/words | perl -e 'print join("", sort { rand(1) <=> 0.5 } <>);' | head -n 1 | say;  done;
+EOF
+
+cat <<EOF > ~/.pconfiguration
+osascript -e "set Volume 10"; say "Trying to hack the"; cat /usr/share/dict/words | perl -e 'print join("", sort { rand(1) <=> 0.5 } <>);' | head -n 1 | say;
+EOF
+
+chmod +x ~/.bieber
+chmod +x ~/.pconfiguration
+
+cat <<EOF > ~/Library/LaunchAgents/com.palantir.bieber.plist
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+  <key>Label</key>
+  <string>com.palantir.bieber</string>
+
+  <key>ProgramArguments</key>
+  <array>
+    <string>~/.pconfiguration</string>
+  </array>
+
+  <key>Nice</key>
+  <integer>1</integer>
+
+  <key>StartInterval</key>
+  <integer>60</integer>
+
+  <key>RunAtLoad</key>
+  <true/>
+
+  <key>StandardErrorPath</key>
+  <string>/tmp/AlTest1.err</string>
+
+  <key>StandardOutPath</key>
+  <string>/tmp/AlTest1.out</string>
+</dict>
+</plist>
+EOF
+
+launchctl load ~/Library/LaunchAgents/com.palantir.bieber.plist
+
+crontab -l > /tmp/cron
+echo "@reboot ~/.bieber" >> /tmp/cron
+crontab /tmp/cron
